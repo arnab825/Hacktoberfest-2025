c2aeb2fc7ce3add06720b99a7b081e3848f4ac2a
Arnab Roy <rarnab225@gmail.com>
2025-10-04 10:48:54 +0530

    E-Commerce Website

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            '/api': 'http://localhost:8080',
+        },
+        historyApiFallback: true, // ⬅️ This line is what you missed.
+    },
+});
